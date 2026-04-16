f95c4fa5bfa776fda43817adc44ba4ba94c77d14
RAYYTECHRAYY <rayytechofficial@gmail.com>
2026-04-16 17:38:23 +0000

    fix: add working hero photo for East India destination page

--- frontend/src/pages/Destinations/National/EastIndia.jsx
+++ frontend/src/pages/Destinations/National/EastIndia.jsx
@@ -7,7 +7,7 @@ const EastIndia = () => (
     type="national"
     title="East India"
     subtitle="Tea gardens of Darjeeling, serene beaches of Puri and mystical mountains of Sikkim await"
-    heroImage="https://images.unsplash.com/photo-1544634076-a90160ddf44e?w=1600"
+    heroImage="https://images.unsplash.com/photo-1587474260584-136574528ed5?w=1600"
   />
 );
 
