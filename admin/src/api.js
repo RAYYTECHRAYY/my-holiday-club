2a187907a4d0166ed7febf15bde50119fa6477ce
RAYYTECHRAYY <rayytechofficial@gmail.com>
2026-04-16 16:36:49 +0000

    fix: switch backend URL from Render to Hostinger server

--- admin/src/api.js
+++ admin/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: process.env.NODE_ENV === 'production'
-    ? 'https://my-holiday-club.onrender.com'
+    ? 'http://187.127.145.211:8080'
     : '',
 });
 
